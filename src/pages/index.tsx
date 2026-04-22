299f4438c88ae08d048a686a8bd75a2f9a4b1efe
mschndl <massimilianoschindele@gmail.com>
2026-04-22 12:11:53 +0200

    chore: clean up default starter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,11 @@
-import { Link } from 'waku';
-import { Counter } from '../components/counter';
-
 export default async function HomePage() {
-  const data = await getData();
-
   return (
-    <div>
-      <title>{data.title}</title>
-      <h1 className="text-4xl font-bold tracking-tight">{data.headline}</h1>
-      <p>{data.body}</p>
-      <Counter />
-      <Link to="/about" className="mt-4 inline-block underline">
-        About page
-      </Link>
-    </div>
+    <main>
+      <h1>Chengyu Daily</h1>
+    </main>
   );
 }
 
-const getData = async () => {
-  const data = {
-    title: 'Waku',
-    headline: 'Waku',
-    body: 'Hello world!',
-  };
-
-  return data;
-};
-
 export const getConfig = async () => {
-  return {
-    render: 'static',
-  } as const;
+  return { render: 'dynamic' } as const;
 };
